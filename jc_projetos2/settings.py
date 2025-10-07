e08c86459f40ffad0030c94f225862a838e1ac4f
Adriano-Dev22 <caas@cesar.school>
2025-10-07 19:33:35 -0300

    Update settings.py

--- jc_projetos2/settings.py
+++ jc_projetos2/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
     
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
