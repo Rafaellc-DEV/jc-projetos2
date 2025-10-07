e19ad141cd5c6b950bc6b1d2e3ac8922b5ff2f49
Adriano-Dev22 <caas@cesar.school>
2025-10-07 19:20:21 -0300

    Update urls.py

--- noticias/urls.py
+++ noticias/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register_view, name="register"), 
+    path("premium/", views.conteudo_premium, name="conteudo_premium"),
 ]
